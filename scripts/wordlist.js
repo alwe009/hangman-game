55cb130763c3b7ca8af8952affc84e3a82ae9e7a
alwe009 <63882279+alwe009@users.noreply.github.com>
2023-07-15 23:52:56 +0800

    first commit

---
+++ scripts/wordlist.js
@@ -0,0 +1,142 @@
+const wordList = [
+  {
+    word: "government",
+    hint: "The governing body of a nation, state, or community.",
+  },
+  { word: "president", hint: "The elected head of a republic." },
+  {
+    word: "community",
+    hint: "A group of people living in the same place or having a particular characteristic in common.",
+  },
+  {
+    word: "population",
+    hint: "All the inhabitants of a particular town, area, or country.",
+  },
+  {
+    word: "university",
+    hint: "An educational institution designed for instruction, examination, or both, of students in many branches of advanced learning, conferring degrees in various faculties, and often embodying colleges and similar institutions.",
+  },
+  {
+    word: "information",
+    hint: "Facts provided or learned about something or someone.",
+  },
+  {
+    word: "development",
+    hint: "The process of developing or being developed.",
+  },
+  {
+    word: "management",
+    hint: "The process of dealing with or controlling things or people.",
+  },
+  {
+    word: "environment",
+    hint: "The surroundings or conditions in which a person, animal, or plant lives or operates.",
+  },
+  {
+    word: "technology",
+    hint: "The application of scientific knowledge for practical purposes, especially in industry.",
+  },
+  {
+    word: "communication",
+    hint: "The imparting or exchanging of information or news.",
+  },
+  {
+    word: "opportunity",
+    hint: "A set of circumstances that makes it possible to do something.",
+  },
+  {
+    word: "experience",
+    hint: "Practical contact with and observation of facts or events.",
+  },
+  {
+    word: "relationship",
+    hint: "The way in which two or more concepts, objects, or people are connected or the state of being connected.",
+  },
+  {
+    word: "education",
+    hint: "The process of receiving or giving systematic instruction, especially at a school or university.",
+  },
+  {
+    word: "organization",
+    hint: "An organized group of people with a particular purpose, such as a business or government department.",
+  },
+  { word: "requirement", hint: "A thing that is necessary or wanted." },
+  {
+    word: "department",
+    hint: "A division of a large organization such as a government, university, or business, dealing with a specific area of activity.",
+  },
+  {
+    word: "discussion",
+    hint: "The action or process of talking about something in order to reach a decision or to exchange ideas.",
+  },
+  {
+    word: "responsibility",
+    hint: "The state or fact of having a duty to deal with something or of having control over someone.",
+  },
+  { word: "possibility", hint: "A thing that may happen or be the case." },
+  {
+    word: "difference",
+    hint: "A point or way in which people or things are dissimilar.",
+  },
+  {
+    word: "construction",
+    hint: "The building of something, typically a large structure.",
+  },
+  {
+    word: "performance",
+    hint: "The action or process of carrying out or accomplishing an action, task, or function.",
+  },
+  { word: "examination", hint: "A detailed inspection or investigation." },
+  { word: "introduction", hint: "The action of introducing something." },
+  {
+    word: "competition",
+    hint: "The activity or condition of striving to gain or win something by defeating or establishing superiority over others.",
+  },
+  { word: "resolution", hint: "A firm decision to do or not to do something." },
+  {
+    word: "application",
+    hint: "A formal request to an authority for something.",
+  },
+  {
+    word: "contribution",
+    hint: "The part played by a person or thing in bringing about a result or helping something to advance.",
+  },
+  {
+    word: "description",
+    hint: "A spoken or written representation or account of a person, object, or event.",
+  },
+  {
+    word: "establishment",
+    hint: "The action of establishing something or being established.",
+  },
+  { word: "improvement", hint: "The action of improving or being improved." },
+  {
+    word: "recommendation",
+    hint: "A suggestion or proposal as to the best course of action, especially one put forward by an authoritative body.",
+  },
+  {
+    word: "satisfaction",
+    hint: "Fulfillment of one's wishes, expectations, or needs, or the pleasure derived from this.",
+  },
+  {
+    word: "administration",
+    hint: "The process or activity of running a business, organization, etc.",
+  },
+  {
+    word: "understanding",
+    hint: "The ability to understand something; comprehension.",
+  },
+  {
+    word: "investigation",
+    hint: "The action of investigating something or someone; formal or systematic examination or research.",
+  },
+  {
+    word: "concentration",
+    hint: "The action or power of focusing one's attention or mental effort.",
+  },
+  {
+    word: "preparation",
+    hint: "The action or process of making ready or being made ready for use or consideration.",
+  },
+  { word: "determination", hint: "Firmness of purpose; resoluteness." },
+];
